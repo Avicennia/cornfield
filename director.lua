fdf633a9220c89e9c14046e79b5d3b280c58c1e5
Seth Hunter <a.germinans@gmail.com>
2020-05-25 10:50:13 -0700

    Trivial code beautification
    - Ignore

--- director.lua
+++ director.lua
@@ -2,48 +2,48 @@
 
 minetest.register_node(tm.."director",{
 
-    description = "Director",
-    tiles = {"glass.png"},
-    paramtype = "light",
-    paramtype2 = "facedir",
-    drawtype = "mesh",
-    sunlight_propagates = true,
-    mesh = "director.obj",
-    groups = {oddly_breakable_by_hand = 1},
-    on_punch = function(pos)
-        minetest.remove_node(pos)
-    end
+        description = "Director",
+        tiles = {"glass.png"},
+        paramtype = "light",
+        paramtype2 = "facedir",
+        drawtype = "mesh",
+        sunlight_propagates = true,
+        mesh = "director.obj",
+        groups = {oddly_breakable_by_hand = 1},
+        on_punch = function(pos)
+            minetest.remove_node(pos)
+        end
 
 })
 minetest.register_node(tm.."director_charged",{
 
-    description = "Director",
-    tiles = {"glass.png^nc_optics_port_active.png"},
-    paramtype = "light",
-    paramtype2 = "facedir",
-    drawtype = "mesh",
-    sunlight_propagates = true,
-    mesh = "director.obj",
-    groups = {oddly_breakable_by_hand = 1, cf_charge = 1},
-    on_construct = function(pos)
-        local node = minetest.get_node(pos)
-        local face = nodecore.facedirs[node.param2]
-        local ahe = vector.add(face.k, pos)
-        local tn = minetest.get_node(ahe)
-        local out_b = vector.add(vector.multiply(face.k, {x = -1, y = 1, z = -1}), pos)
-        local nt = minetest.get_node(out_b)
+        description = "Director",
+        tiles = {"glass.png^nc_optics_port_active.png"},
+        paramtype = "light",
+        paramtype2 = "facedir",
+        drawtype = "mesh",
+        sunlight_propagates = true,
+        mesh = "director.obj",
+        groups = {oddly_breakable_by_hand = 1, cf_charge = 1},
+        on_construct = function(pos)
+            local node = minetest.get_node(pos)
+            local face = nodecore.facedirs[node.param2]
+            local ahe = vector.add(face.k, pos)
+            local tn = minetest.get_node(ahe)
+            local out_b = vector.add(vector.multiply(face.k, {x = -1, y = 1, z = -1}), pos)
+            local nt = minetest.get_node(out_b)
 
-        cf.spark1({x = pos.x, y = pos.y, z = pos.z})
-        if(minetest.get_node_group(tn.name, "cf_charge") > 0) then
-        local meta = minetest.get_meta(ahe)
-        local mmeta = minetest.get_meta(pos)
-        meta:set_int("cf_charge", meta:get_int("cf_charge") + minetest.registered_nodes[nt.name]["light_source"])
+            cf.spark1({x = pos.x, y = pos.y, z = pos.z})
+            if(minetest.get_node_group(tn.name, "cf_charge") > 0) then
+                local meta = minetest.get_meta(ahe)
+                local mmeta = minetest.get_meta(pos)
+                meta:set_int("cf_charge", meta:get_int("cf_charge") + minetest.registered_nodes[nt.name]["light_source"])
 
-        else end
-        minetest.after(0.1, function() minetest.set_node(pos, {name = tm.."director", param2 = minetest.get_node(pos).param2}) end)
-    end,
-    on_punch = function(pos)
-        minetest.remove_node(pos)
+            else end
+            minetest.after(0.1, function() minetest.set_node(pos, {name = tm.."director", param2 = minetest.get_node(pos).param2}) end)
+        end,
+        on_punch = function(pos)
+            minetest.remove_node(pos)
         end
 
 })
--- dock.lua
+++ dock.lua
@@ -2,36 +2,36 @@
 
 function cf.portalBuild(pos, rad, is_fill)
     if(not is_fill)then
-    local poses = {{x = pos.x + rad, y = pos.y, z = pos.z + rad},
-    {x = pos.x - rad, y = pos.y, z = pos.z - rad},
-    {x = pos.x - rad, y = pos.y, z = pos.z + rad},
-    {x = pos.x + rad, y = pos.y, z = pos.z - rad}}
+        local poses = {{x = pos.x + rad, y = pos.y, z = pos.z + rad},
+            {x = pos.x - rad, y = pos.y, z = pos.z - rad},
+            {x = pos.x - rad, y = pos.y, z = pos.z + rad},
+            {x = pos.x + rad, y = pos.y, z = pos.z - rad}}
 
-    for n = 1, 4, 1 do
-    minetest.set_node(poses[n], {name = tm.."barrier_a"})
-    end
-    
-    local pool = {2,2,-1,-3}
-    for n = 1, 4, 1 do
-    local no = pool[n] + n
-    
-    local ar = minetest.find_nodes_in_area(poses[n],poses[no], "air")
-    if(ar and #ar >= 1)then
-        for a = 1, #ar, 1 do
-        minetest.set_node(ar[a], {name = tm.."barrier_a"})
+        for n = 1, 4, 1 do
+            minetest.set_node(poses[n], {name = tm.."barrier_a"})
         end
-    else end
+
+        local pool = {2,2,-1,-3}
+        for n = 1, 4, 1 do
+            local no = pool[n] + n
+
+            local ar = minetest.find_nodes_in_area(poses[n],poses[no], "air")
+            if(ar and #ar >= 1)then
+                for a = 1, #ar, 1 do
+                    minetest.set_node(ar[a], {name = tm.."barrier_a"})
+                end
+            else end
 
 
-    
-    end
+
+        end
 
 
-elseif(is_fill == true)then -- For Roofing things, like Jaycee does.
+    elseif(is_fill == true)then -- For Roofing things, like Jaycee does.
         local ar = minetest.find_nodes_in_area({x = pos.x + rad, y = pos.y, z = pos.z + rad},{x = pos.x - rad, y = pos.y, z = pos.z - rad}, "air")
         if(ar and #ar >= 1)then
             for a = 1, #ar, 1 do
-            minetest.set_node(ar[a], {name = tm.."barrier_a"})
+                minetest.set_node(ar[a], {name = tm.."barrier_a"})
             end
         else end
     else
@@ -59,22 +59,22 @@ end
 
 minetest.register_node(tm.."port",{
 
-    description = "Port",
-    tiles = {"devtex.png"},
-    drawtype = "mesh",
-    paramtype = "light",
-    mesh = "port.obj",
-    sunlight_propagates = true,
-    groups = {oddly_breakable_by_hand = 1},
-    on_construct = function (pos)
-    minetest.get_node_timer(pos):start(0.5)
-    end,
-    on_timer = function(pos)
-    end,
-        
-    on_punch = function(pos)
-        minetest.set_node(pos, {name = tm.."port_occ"})
-    end
+        description = "Port",
+        tiles = {"devtex.png"},
+        drawtype = "mesh",
+        paramtype = "light",
+        mesh = "port.obj",
+        sunlight_propagates = true,
+        groups = {oddly_breakable_by_hand = 1},
+        on_construct = function (pos)
+            minetest.get_node_timer(pos):start(0.5)
+        end,
+        on_timer = function(pos)
+        end,
+
+        on_punch = function(pos)
+            minetest.set_node(pos, {name = tm.."port_occ"})
+        end
 
 })
 
@@ -112,62 +112,62 @@ end
 
 minetest.register_node(tm.."port_occ",{
 
-    description = "Occupied Port",
-    tiles = {"devtex2.png"},
-    drawtype = "mesh",
-    paramtype = "light",
-    sunlight_propagates = true,
-    mesh = "port_occ.obj",
-    groups = {oddly_breakable_by_hand = 1},
-    on_punch = function(pos,_,puncher)
-    local rad = 26
-        if(puncher:get_player_control().sneak == false)then
-        local thresh = 10
-        local below = {x=pos.x,y=pos.y-1,z=pos.z}
-        if(minetest.get_meta(below):get_int("cf_charge") >= thresh)then
+        description = "Occupied Port",
+        tiles = {"devtex2.png"},
+        drawtype = "mesh",
+        paramtype = "light",
+        sunlight_propagates = true,
+        mesh = "port_occ.obj",
+        groups = {oddly_breakable_by_hand = 1},
+        on_punch = function(pos,_,puncher)
+            local rad = 26
+            if(puncher:get_player_control().sneak == false)then
+                local thresh = 10
+                local below = {x=pos.x,y=pos.y-1,z=pos.z}
+                if(minetest.get_meta(below):get_int("cf_charge") >= thresh)then
 
-        
-        minetest.get_node_timer(pos):start(0.5)
-    else minetest.chat_send_all("why")
-    end
-elseif(puncher:get_player_control().sneak == true)then
-    minetest.get_meta(pos):set_int("cf_radius", rad)
-else
-end
-    end,
-    on_construct = function (pos)
-        local rad = 26
-        minetest.get_meta(pos):set_int("cf_radius", rad)
-        minetest.get_node_timer(pos):start(0.5)
-    end,
-    on_timer = function(pos)
-        local meta = minetest.get_meta(pos)
-        local rad = meta:get_int("cf_radius")
-        
-        barrier_wax(pos,rad)
-        minetest.get_node_timer(pos):start(3.5)
-    end,
-    on_rightclick = function(pos)
-    minetest.remove_node(pos)
-    end
+
+                    minetest.get_node_timer(pos):start(0.5)
+                else minetest.chat_send_all("why")
+                end
+            elseif(puncher:get_player_control().sneak == true)then
+                minetest.get_meta(pos):set_int("cf_radius", rad)
+            else
+            end
+        end,
+        on_construct = function (pos)
+            local rad = 26
+            minetest.get_meta(pos):set_int("cf_radius", rad)
+            minetest.get_node_timer(pos):start(0.5)
+        end,
+        on_timer = function(pos)
+            local meta = minetest.get_meta(pos)
+            local rad = meta:get_int("cf_radius")
+
+            barrier_wax(pos,rad)
+            minetest.get_node_timer(pos):start(3.5)
+        end,
+        on_rightclick = function(pos)
+            minetest.remove_node(pos)
+        end
 
 })
 
 minetest.register_abm(
-{
-    label = "Barrier Filling",
-    
-    nodenames = {tm.."port_occ"},
-    
-    neighbors = {"air"},
-   
-    interval = 1.0,
-    chance = 1,
-    
-    catch_up = false,
-    
-    action = function(pos)
-        local meta = minetest.get_meta(pos)
+    {
+        label = "Barrier Filling",
+
+        nodenames = {tm.."port_occ"},
+
+        neighbors = {"air"},
+
+        interval = 1.0,
+        chance = 1,
+
+        catch_up = false,
+
+        action = function(pos)
+            local meta = minetest.get_meta(pos)
             entomb(pos,minetest.get_meta(pos):get_int("cf_radius"))
-    end
-})
+        end
+    })
